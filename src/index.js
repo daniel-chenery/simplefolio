5d2ae54e36bce7ccc220afe8a46dd5b8a2665e02
Daniel Chenery <daniel@itsdaniel0.com>
2024-05-13 14:14:07 +0100

    Removed gravatar due to third-party cookies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
-import { gravatar } from './scripts/gravatar';
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
-
-gravatar(document.querySelector('img[alt="Profile Image"]'), 'daniel@itsdaniel0.com');
