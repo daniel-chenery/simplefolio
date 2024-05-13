f70f8d4928b7df2841d21c627c41508cafe691b0
Daniel Chenery <daniel@itsdaniel0.com>
2024-05-13 15:14:13 +0100

    Remove projects section

--- src/data/scrollRevealConfig.js
+++ src/data/scrollRevealConfig.js
@@ -44,20 +44,6 @@ export const targetElements = [
     },
   },
   {
-    element: ".project-wrapper__text",
-    animation: {
-      delay: 500,
-      origin: window.innerWidth > 768 ? "left" : "bottom",
-    },
-  },
-  {
-    element: ".project-wrapper__image",
-    animation: {
-      delay: 1000,
-      origin: window.innerWidth > 768 ? "right" : "bottom",
-    },
-  },
-  {
     element: ".contact-wrapper",
     animation: {
       delay: 800,
