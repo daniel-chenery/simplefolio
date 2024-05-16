193045370a8f1667236c6b79215252ac26686516
Daniel Chenery <daniel@itsdaniel0.com>
2024-05-16 18:14:02 +0100

    Toggle scrolling list on/off

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 import { initialiseMarquee } from "./scripts/marquee";
+import { SkillList } from './scripts/list';
 
 initialiseMarquee();
 initScrollReveal(targetElements, defaultProps);
+
+new SkillList().initalise();
--- src/scripts/list.ts
+++ src/scripts/list.ts
@@ -1,4 +1,44 @@
-const target = document.querySelector('[href="#list-view"]');
-target?.addEventListener('click', () => {
-    document.querySelector('skill__list')?.classList.add('skill__list--stacked');
-});
+export class SkillList {
+    private _wrapper = 'skill__wrapper';
+    private _stacked = '--stacked';
+    private _target: HTMLAnchorElement | null = document.querySelector('[href="#list-view"]');
+
+    public isStacked: boolean = false;
+
+    public initalise(): void {
+        this._target?.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            if (this.isStacked) {
+                this.unstack();
+            } else {
+                this.stack();
+            }
+
+        });
+    }
+
+    private stack(): void {
+        const tree = document.querySelector(`.${this._wrapper}`);
+        tree?.classList.add(`${this._wrapper}${this._stacked}`)
+
+        if (this._target) {
+            this._target.text = 'grid_off';
+        }
+
+        this.isStacked = true;
+    }
+
+    private unstack(): void {
+        const tree = document.querySelector(`.${this._wrapper}`);
+        tree?.classList.remove(`${this._wrapper}${this._stacked}`)
+
+        if (this._target) {
+            this._target.text = 'grid_on';
+        }
+
+        this.isStacked = false;
+    }
+
+
+}
