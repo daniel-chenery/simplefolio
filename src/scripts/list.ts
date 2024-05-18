d39e5bf7a6d9e1dd2beed78b92c5e68e5945c297
Daniel Chenery <daniel@itsdaniel0.com>
2024-05-18 09:32:31 +0100

    Make the whole text clickable to stop the grid

--- src/scripts/list.ts
+++ src/scripts/list.ts
@@ -19,25 +19,33 @@ export class SkillList {
     }
 
     private stack(): void {
+        const icon = this.getIcon();
         const tree = document.querySelector(`.${this._wrapper}`);
         tree?.classList.add(`${this._wrapper}${this._stacked}`)
 
-        if (this._target) {
-            this._target.text = 'grid_off';
+        if (icon) {
+            icon.textContent = 'grid_off';
         }
 
         this.isStacked = true;
     }
 
     private unstack(): void {
+        const icon = this.getIcon();
         const tree = document.querySelector(`.${this._wrapper}`);
         tree?.classList.remove(`${this._wrapper}${this._stacked}`)
 
-        if (this._target) {
-            this._target.text = 'block';
+        if (icon) {
+            icon.textContent = 'block';
         }
 
         this.isStacked = false;
+    }
+
+    private getIcon(): HTMLElement | null | undefined {
+        return this._target?.classList.contains('material-symbols-outlined') ?
+            this._target :
+            this._target?.querySelector('.material-symbols-outlined');
     }
 
 
